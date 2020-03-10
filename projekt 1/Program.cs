fddec68bec3c2925b52886c131af342cf886aa89
Madiken Volkova <madikenv@gmail.com>
2020-03-10 17:33:54 +0300

    С#

---
+++ projekt 1/Program.cs
@@ -0,0 +1,24 @@
+﻿//namespace projekt_1
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace Первая_попытка_С1
+    // Реализовать функцию возведения в квадрат произведения двух вещественных чисел 
+
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                double a, b, c;
+                a = Convert.ToInt32(Console.ReadLine());
+                b = Convert.ToInt32(Console.ReadLine());
+                c = Math.Pow(a * b, 2);
+                Console.WriteLine("c - " + c);
+            }
+        }
+    }
